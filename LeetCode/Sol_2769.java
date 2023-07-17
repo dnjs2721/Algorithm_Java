52fbde24fad6f3e2bdc835add7e541ce78d3ce92
dnjs2721 <skadu66@gmail.com>
2023-07-17 21:45:50 +0900

    Find the Maximum Achievable Number

---
+++ LeetCode/Sol_2769.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+public class Sol_2769 {
+    public static int theMaximumAchievableX(int num, int t) {
+        return num + t + t;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(theMaximumAchievableX(3, 2));
+    }
+}
