9d4e3e002ce1a84a5429c9a9e6e7a5d331dddf1a
dnjs2721 <skadu66@gmail.com>
2023-08-25 00:03:07 +0900

    설탕 배달

--- BaekJoon/설탕_배달.java
+++ BaekJoon/설탕_배달.java
@@ -3,7 +3,6 @@ package BaekJoon;
 import java.util.*;
 
 public class 설탕_배달 {
-    static int[] pack = new int[]{5, 3};
     static int answer = -1;
 
     public static void main(String[] args) {
@@ -14,34 +13,19 @@ public class 설탕_배달 {
     }
 
     public static int solution(int n) {
-        if (n % 5 == 0) {
-            return n / 5;
-        }
-        dp(n, 0);
+        check(n, 0);
         return answer;
     }
 
-    public static void dp(int n, int count) {
-        if (n == 0) {
-            if (answer == -1) {
-                answer = count;
-            } else {
-                answer = Math.min(answer, count);
-            }
-            return;
-        } else if (n < 3) {
+    public static void check(int n, int count) {
+        if (n % 5 == 0) {
+            answer = count + n / 5;
             return;
         }
-        for (int i : pack) {
-            if (answer == -1) {
-                dp(n - i, count + 1);
-            } else {
-                if (answer <= count + 1) {
-                    return;
-                } else {
-                    dp(n - i, count + 1);
-                }
-            }
+        if (n < 0) {
+            answer = -1;
+            return;
         }
+        check(n - 3, count + 1);
     }
 }
