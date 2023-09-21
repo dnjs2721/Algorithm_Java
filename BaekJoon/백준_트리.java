57eb94c9f8b03d86e020ad01d57d851d6ea5ade9
dnjs2721 <skadu66@gmail.com>
2023-09-21 23:13:57 +0900

    트리 주석 추가

--- BaekJoon/백준_트리.java
+++ BaekJoon/백준_트리.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class 백준_트리 {
 
     static List<List<Integer>> map;
-    static Set<Integer> find = new TreeSet<>((o1, o2) -> o2 - o1);
+    static Set<Integer> find = new TreeSet<>((o1, o2) -> o2 - o1); // 삭제를 위해 큰 인덱스 부터 나열
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,26 +26,25 @@ public class 백준_트리 {
         }
 
         dfs(Integer.parseInt(br.readLine()));
-        System.out.println(map);
         for (int node : find) {
             map.remove(node);
         }
-        System.out.println(map);
 
-        if (map.size() == 1) {
+        if (map.size() == 1) { // 노드가 한개 남았다면 리프 노드
             System.out.println(1);
         } else {
             int count = 0;
-            for (List<Integer> m : map) {
-                if (m.isEmpty()) count++;
-                if (m.size() == 1 && m.get(0) >= map.size()) count++;
+            for (List<Integer> m : map) { // 루트가 지워졌다면 0을 출력
+                if (m.isEmpty()) count++; // 자식이 없다면 리프노드
+                if (m.size() == 1 && m.get(0) >= map.size()) count++; // 자식이 하나 있지만 지워진 자식이라면 리프노드
+                // 자식이 두개 이상 이라면 한 자식이 지워져도 리프노드가 될 수 없다.
             }
             System.out.println(count);
         }
     }
 
     public static void dfs(int start) {
-        find.add(start);
+        find.add(start); // 자식 인덱스를 모두 찾아 보관
         for (int child : map.get(start)) {
             dfs(child);
         }
