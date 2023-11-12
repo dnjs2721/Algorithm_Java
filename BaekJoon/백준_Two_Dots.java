801b3d2fc93bba0423d4f909e31f88db7f670390
dnjs2721 <skadu66@gmail.com>
2023-11-12 19:19:41 +0900

    Two Dots

---
+++ BaekJoon/백준_Two_Dots.java
@@ -0,0 +1,60 @@
+package BaekJoon;
+
+import java.util.*;
+import java.io.*;
+
+public class 백준_Two_Dots {
+    static int n, m;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dx = new int[]{0, 0, -1, 1};
+    static int[] dy = new int[]{-1, 1, 0 , 0};
+    static int startX, startY;
+    static boolean flag = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new char[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (flag) break;
+                startX = j;
+                startY = i;
+                dfs(j, i, map[i][j], 1);
+            }
+        }
+
+        if (flag) System.out.println("Yes");
+        else System.out.println("No");
+    }
+
+    public static void dfs(int x, int y, char color, int depth) {
+        visited[y][x] = true;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+            if (map[ny][nx] != color) continue;
+            if (depth + 1 >= 4 && nx == startX && ny == startY) {
+                flag = true;
+                return;
+            }
+            if (visited[ny][nx]) continue;
+            dfs(nx, ny, color, depth + 1);
+        }
+        visited[y][x] = false;
+    }
+}
