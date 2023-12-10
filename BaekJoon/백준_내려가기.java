c08e1b260ce09d482423485776f72a021dd8c8c0
dnjs2721 <skadu66@gmail.com>
2023-12-10 21:18:09 +0900

    백준 내려가기 - 다른 풀이 추가

--- BaekJoon/백준_내려가기.java
+++ BaekJoon/백준_내려가기.java
@@ -46,10 +46,48 @@ public class 백준_내려가기 {
             }
         }
 
-        // 마지막 줄 중 가장 큰 점수
         int max = Arrays.stream(dp[0][n - 1]).max().orElse(0);
         // 마지막 줄 중 가장 작은 점수
         int min = Arrays.stream(dp[1][n - 1]).min().orElse(0);
+
+        System.out.printf("%d %d", max, min);
+    }
+
+    // 가장 적은 메모리와 가장 빠른 시간
+    public static void main1(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] dp = new int[2][3];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int n0 = Integer.parseInt(st.nextToken());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+
+            int max0 = n0 + Math.max(dp[0][0], dp[0][1]);
+            int min0 = n0 + Math.min(dp[1][0], dp[1][1]);
+
+            int max1 = n1 + Math.max(dp[0][0], Math.max(dp[0][1], dp[0][2]));
+            int min1 = n1 + Math.min(dp[1][0], Math.min(dp[1][1], dp[1][2]));
+
+            int max2 = n2 + Math.max(dp[0][1], dp[0][2]);
+            int min2 = n2 + Math.min(dp[1][1], dp[1][2]);
+
+            dp[0][0] = max0;
+            dp[0][1] = max1;
+            dp[0][2] = max2;
+            dp[1][0] = min0;
+            dp[1][1] = min1;
+            dp[1][2] = min2;
+        }
+
+
+        int max = Arrays.stream(dp[0]).max().orElse(0);
+        int min = Arrays.stream(dp[1]).min().orElse(0);
 
         System.out.printf("%d %d", max, min);
     }
